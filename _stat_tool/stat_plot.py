814d24076508a8cd10766c6e3d91f163abd53f14
Wojciech Piatkowski <wojciech.piatkowski.dev@gmail.com>
2024-01-28 14:25:37 +0100

    More stats and screenshots

--- _stat_tool/stat_plot.py
+++ _stat_tool/stat_plot.py
@@ -1,9 +1,23 @@
 import json
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 events_key = "events"
 frame_count_key = "frame_count"
 records_key = "records"
+
+pathtracer_keys: list[str] = [
+    # 1
+    "/onFrameRender/RenderGraphExe::execute()/MinimalPathTracer/cpu_time",
+    # 2
+    "/onFrameRender/RenderGraphExe::execute()/MinimalPathTracer/gpu_time",
+    # 3
+    "/onFrameRender/RenderGraphExe::execute()"
+    "/MinimalPathTracer/raytraceScene/cpu_time",
+    # 4
+    "/onFrameRender/RenderGraphExe::execute()"
+    "/MinimalPathTracer/raytraceScene/gpu_time"
+]
 
 restir_keys: list[str] = [
     # 1
@@ -31,6 +45,9 @@ def create_plot(x_axes: list[list], y_axes: list[list], labels: list[str],
     (fig, ax) = plt.subplots(1, 1)
     ax.set_title(title)
     ax.grid(True)
+    ax.set_xlabel("Frame")
+    ax.set_ylabel("Time [ms]")
+    ax.yaxis.set_major_formatter(FormatStrFormatter("%.2f"))
 
     num = len(x_axes)
     for i in range(num):
@@ -49,7 +66,7 @@ def create_plot(x_axes: list[list], y_axes: list[list], labels: list[str],
 
 
 def main():
-    with open("./test_profile.json") as file:
+    with open("./restir_static_1d_4p_1.json") as file:
         data = json.load(file)
 
     frame_count = data[frame_count_key]
@@ -60,7 +77,6 @@ def main():
     x_frame_num = [i + 1 for i in range(frame_count)]
 
     create_plot([x_frame_num], [records_1], ["ReSTIR"], ['b'], "CPU Time")
-
 
 
 if __name__ == "__main__":
